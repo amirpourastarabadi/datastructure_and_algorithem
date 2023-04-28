ee5b847199008d7ebe4802d20e5b28e2ca7e6bbc
amirpourastarabadi <amirpourastarabadi@gmail.com>
2023-04-28 20:19:43 +0330

    min and max heap

--- data_structures/heap/heap.py
+++ data_structures/heap/heap.py
@@ -1,4 +1,6 @@
-class Heap:
+from abc import ABC, abstractmethod
+
+class Heap(ABC):
     def __init__(self, values = []) -> None:
         self.root = None
         self.nodes = [None]
@@ -6,13 +8,37 @@ class Heap:
         
         for item in values:
             self.insert(item)
-        
-    def minify(self):
-        pass
-
-    def maxify():
+    
+    @abstractmethod
+    def insert(self, value):
         pass
     
+    @abstractmethod
+    def get_top(self):
+        pass
+
+    def get_size(self):
+        return self.size - 1
+
+    def __str__(self) -> str:
+        result = ''
+        for i in range(1, self.get_size()):
+            result += str(self.nodes[i]) + ', '
+        return result
+
+    def find_parent(self, child_index):
+        return child_index // 2
+    
+    def find_siblings(self, parent):
+        s1 = 2 * parent if (2 * parent < len(self.nodes)) else None
+        s2 = s1 + 1 if (2 * parent + 1 < len(self.nodes)) else None
+        return s1, s2
+    
+class MaxHeap(Heap):
+
+    def __init__(self, values=[]) -> None:
+        super().__init__(values)
+        
     def insert(self, value):
         self.nodes.append(value)
         value_index = self.size
@@ -22,20 +48,8 @@ class Heap:
             self.nodes[value_index], self.nodes[parent_index] = self.nodes[parent_index], self.nodes[value_index]
             value_index = parent_index
             parent_index = self.find_parent(value_index)
-    
-    def find_parent(self, child_index):
-        return child_index // 2
-    
-    def __str__(self) -> str:
-        result = ''
-        for i in self.nodes:
-            result += str(i) + ', '
-        return result
-    
-    def get_max(self):
-        return self.nodes[1]
-    
-    def get_max(self):
+
+    def get_top(self):
         if self.size <= 1:
             raise Exception("Heap Is Empty.")
         
@@ -65,27 +79,64 @@ class Heap:
         self.size -= 1
         return head 
     
-    def find_siblings(self, parent):
-        s1 = 2 * parent if (2 * parent < len(self.nodes)) else None
-        s2 = s1 + 1 if (2 * parent + 1 < len(self.nodes)) else None
-        return s1, s2
-     
-h = Heap()
-h.insert(10)
-h.insert(20)
-h.insert(15)
-h.insert(5)
-h.insert(1)
-h.insert(4)
-print(h)
+class MinHeap(Heap):
 
-for _ in range(6):
-    print(h.get_max())
-    print(h)
+    def __init__(self, values=[]) -> None:
+        super().__init__(values)
+        
+    def insert(self, value):
+        self.nodes.append(value)
+        value_index = self.size
+        self.size += 1
+        parent_index = self.find_parent(value_index) 
+        while value_index > 1 and self.nodes[value_index] < self.nodes[parent_index]:
+            self.nodes[value_index], self.nodes[parent_index] = self.nodes[parent_index], self.nodes[value_index]
+            value_index = parent_index
+            parent_index = self.find_parent(value_index)
 
-h2 = Heap([10,20,15,5,1,4])
-print(h2)
+    def get_top(self):
+        if self.size <= 1:
+            raise Exception("Heap Is Empty.")
+        
+        parent = 1
+        
+        self.nodes[parent], head = self.nodes[self.size - 1], self.nodes[parent]
+        
+        del self.nodes[self.size - 1]
+        
+        siblings = self.find_siblings(parent)
+        while siblings[0] is not None:
+            top_index = parent
 
-# for _ in range(8):
-#     print(h.get_max())
-#     print(h)
+            if self.nodes[parent] > self.nodes[siblings[0]]:
+                top_index = siblings[0]
+
+            if siblings[1] is not None and self.nodes[top_index] > self.nodes[siblings[1]]:
+                top_index = siblings[1]
+            
+            if parent == top_index:
+                break
+
+            self.nodes[parent], self.nodes[top_index] = self.nodes[top_index], self.nodes[parent]
+            parent = top_index
+            siblings = self.find_siblings(parent)
+
+        self.size -= 1
+        return head 
+    
+
+
+print("################# MAX HEAP #################")
+max_heap = MaxHeap([10,20,3,5,15,5,1,4])
+print(max_heap)
+print("get items from top: ")
+for _ in range(max_heap.get_size()):
+    print(max_heap.get_top(), end= '->')
+
+
+print("\n\n################# MIN HEAP #################")
+min_heap = MinHeap([10,20,3,5,15,5,1,4])
+print(min_heap)
+print("get items from top: ")
+for _ in range(min_heap.get_size()):
+    print(min_heap.get_top(), end= '->')
